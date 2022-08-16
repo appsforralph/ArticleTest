1975a3dee90238d16c66fc6cc086fe9fb23fbab5
Ralph Pagulayan <ralph.pagulayan@playngo.com>
2022-08-16 08:50:54 +0800

    Created the structure for the Article API

---
+++ ArticleTest/Model/DTO/ArticleResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArticleTest.Model.DTO
+{
+    public class ArticleResponse
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public List<ArticleData> data { get; set; }
+    }
+
+    public class ArticleData
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string author { get; set; }
+        public int? num_comments { get; set; }
+        public object story_id { get; set; }
+        public string story_title { get; set; }
+        public string story_url { get; set; }
+        public int? parent_id { get; set; }
+        public int created_at { get; set; }
+    }
+}
---
+++ ArticleTest/Services/ArticleService.cs
@@ -0,0 +1,12 @@
+﻿using ArticleTest.Common.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArticleTest.Services
+{
+    public class ArticleService : IArticleService
+    {
+    }
+}
